793c1b8280a13f16d165c55879a7ae1f906ec158
一号已就绪\ttb <18281868192@163.com>
2020-09-18 19:27:57 +0800

    第一次提交

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+//引入vuex持久化插件
+import vuexPresient from "vuex-persistedstate";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  plugins: [vuexPresient()]
 });
